f1b9f3284a86d67757324101a7a6e95811956841
Stephen Collins <sgcollins@outlook.com>
2024-01-11 20:45:06 -0500

    added basic bits for i18n

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  "en-US": "English",
+  es: "Español",
+};
+
+export const defaultLang = "en-US";
+
+export const ui = {
+  "en-US": {
+    "nav.features": "Features",
+    "nav.how-it-works": "How It Works",
+  },
+  es: {
+    "nav.features": "Características",
+    "nav.how-it-works": "Cómo Funciona",
+  },
+} as const;
+
+export const showDefaultLang = false;
